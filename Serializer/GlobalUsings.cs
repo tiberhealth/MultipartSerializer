2edbb2d12d1ebc4d94db652965949da645092a0d
Bryan M Lenihan <blenihan@tiberhealth.com>
2025-03-17 17:53:40 -0400

    Modernize usings

---
+++ Serializer/GlobalUsings.cs
@@ -0,0 +1,14 @@
+// Global using directives
+
+global using System;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.Globalization;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Net.Http.Headers;
+global using System.Reflection;
+global using System.Text.RegularExpressions;
+global using TiberHealth.Serializer.Attributes;
+global using TiberHealth.Serializer.Exceptions;
+global using TiberHealth.Serializer.Extensions;
